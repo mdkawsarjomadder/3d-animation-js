9d7395498d5d548216fd77422d66b69d7106e23f
kawsa-jomadder <mdkawsar342814@gmail.com>
2024-10-24 17:26:35 +0600

    final

--- style/animation.js
+++ style/animation.js
@@ -1,1 +1,20 @@
-console.log("first");
+var products = document.getElementById("productBox");
+var fronts = document.getElementById("frontImg");
+var backs = document.getElementById("backImg");
+
+function cardRight(){
+    products.style.transform ="rotateY(180deg";
+    fronts.style.left = "650px";
+    backs.style.left = "20px";
+    fronts.style.transform = "rotate(-30deg)";
+    backs.style.transform = "rotate(0deg)";
+   
+}
+function cardLeft(){
+    products.style.transform = "rotateY(0deg)";
+    
+   fronts.style.left  = "20px";
+   backs.style.left = "-650px";
+   fronts.style.transform = "rotate(0deg)";
+   backs.style.transform ="rotate(-30deg)";
+}
